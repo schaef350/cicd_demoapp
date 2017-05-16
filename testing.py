54f72d0f11756705fc5160cfc52de70500df7047
schaef350 <schaef350@users.noreply.github.com>
2017-05-16 14:40:48 -0400

    fixed typo

--- testing.py
+++ testing.py
@@ -16,7 +16,7 @@ class FlaskTestCase(unittest.TestCase):
         resp = self.app.get('/hello/world')
         self.assertEquals(resp.data, '"Hello World!"\n')
 
-   def test_universe_correct_http_response(self):
+    def test_universe_correct_http_response(self):
         resp = self.app.get('/hello/universe')
         self.assertEquals(resp.status_code, 200)
 
